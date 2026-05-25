38090f9c35e12bc9acc4b67431bd0f4e411555d7
mekeirish <mmm1747lb@gmail.com>
2026-05-25 06:38:59 +0200

    Mastercode Sync

--- sw.js
+++ sw.js
@@ -1,4 +1,1 @@
-const CACHE = 'v1';
-const assets = ['./', './index.html', './manifest.json', './icon.png'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(assets))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const cacheName='Squaro-v1'; self.addEventListener('install',e=>{e.waitUntil(caches.open(cacheName).then(c=>c.addAll(['./index.html','./manifest.json','./icon.png'])));}); self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
